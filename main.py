39eaa7136b358f9d21d93ec03ab3365a368dbb9b
villa <matias.villalemes@metropolia.fi>
2024-09-17 14:38:38 +0300

    70% valmis

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from funciones import prime_numbers, get_starting_airport, get_destination_airpo
 
 print("\nYou start at a random airport and you gotta travel to another random airport. "
       "\nYou will have available half the km of distance. "
-      "\nTo earn km you can guess the country of your current airport"
+      "\nTo earn km you can guess the country, municipality or height of your current airport"
       "\nor you can roll the dice and play different dice mini games."
-      "\nIf you guess incorrectly you will lose 15% of your current available km."
-      "\nIf you lose at dice, you will lose 10% of your current available km."
-      "\nIf you guess the country you will get 15% of your current available km."
-      "\nIf you win at dice, you will get 10% of your current available km."
-      "\nIf you guess the country correctly, you will sent to a different random airport within your reach"
+      "\nIf you guess incorrectly or lose at a mini game you will lose 25% of your available km"
+      "\nIf you win at dice or guess the airport information correctly, you will get 15% of your current available km."
+      "\nIf you guess the airport information correctly, you will be sent to a different random airport within your reach"
       "\nThe game ends when you earned enough km to reach your destination or you run out of available km.\n"
       "\n\tGood luck!\n")
 
